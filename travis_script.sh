bee67a723fa13b6e788ca9e9efc6e9d808cdc647
Randy Pensinger <rspensinger@gmail.com>
2015-10-18 20:23:54 -0700

    remove broken lines from other project

--- travis_script.sh
+++ travis_script.sh
@@ -2,6 +2,4 @@
 
 set -e
 
-psql -U postgres < src/test/java/io/codemonastery/jdbj/db/postgres_9_4/setup.sql
-mysql -uroot < src/test/java/io/codemonastery/jdbj/db/mysql_5_1/setup.sql
 mvn -B clean test jacoco:report # coveralls:report
